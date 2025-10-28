f976e07042121c63e4452229ace2b4c8cb8d5b28
ox30 <flrnsndz@gmail.com>
2025-10-28 23:35:15 +0100

    Update add_eleve_me.sql

--- add_eleve_me.sql
+++ add_eleve_me.sql
@@ -1,56 +1,3 @@
--- Script pour m'ajouter moi en tant qu'élève:
-INSERT INTO eleves (nom_eleve, prenom_eleve) VALUES
-('Sandoz', 'Florian');
--- Enregistrement de l'id_eleve dans une variable.
-SELECT id_eleve
-INTO @id_eleve
-FROM eleves
-WHERE nom_eleve = 'Sandoz' AND prenom_eleve = 'Florian'
-LIMIT 1;
-
--- Récupération de la formation
-SELECT id_formation
-INTO @id_formation
-FROM formations
-WHERE nom_formation = 'Titre professionnel Développeur web et web mobile'
-LIMIT 1;
-
--- liaison élève avec la formation
-INSERT INTO eleves_formations (id_eleve, id_formation, date_deb_formation, date_fin_formation) VALUES
-(@id_eleve, @id_formation, '2023-09-07', '2026-09-08');
-
--- liaison de la formation avec la matière selon mon cas (Dev_web_mobile avec Gérer une base de données)
--- Récupération de la matière
-SELECT id_matiere
-INTO @id_matiere
-FROM matieres
-WHERE nom_matiere = 'Gérer une base de données'
-LIMIT 1;
-
-INSERT INTO formations_matieres (id_formation, id_matiere, date_deb, date_fin ) VALUES
-(@id_formation, @id_matiere, '2025-10-01', '2025-12-01');
-
--- Ajout du devoir et insertion en varible de celui-ci (Fonctionne quand on veut bien le dernier devoir créé.) 
--- Sinon on peut aussi l'enregistrer en variable (comme avec @id_eleve par exemple) mais je voulais essayer autrement.
-INSERT INTO devoirs (nom_devoir, num_devoir, desc_devoir, id_cours) VALUES
-('Data', '8', "Élaborer des scripts d'alimentation de la base de test", '23');
-SET @id_devoir := LAST_INSERT_ID();
-
--- Création de la Table relationnel eleves_devoirs
-CREATE TABLE IF NOT EXISTS eleves_devoirs (
-  id_eleves_devoir INT NOT NULL AUTO_INCREMENT,
-  id_eleve  INT NOT NULL,
-  id_devoir INT NOT NULL,
-  date_deb  DATE NOT NULL,
-  date_fin  DATE NOT NULL,
-  PRIMARY KEY (id_eleves_devoir),
-  UNIQUE KEY unique_eleve_devoir (id_devoir, id_eleve),
-  KEY idx_ed_eleve (id_eleve),
-  CONSTRAINT fk_ed_eleve  FOREIGN KEY (id_eleve)  REFERENCES eleves(id_eleve)  ON DELETE CASCADE,
-  CONSTRAINT fk_ed_devoir FOREIGN KEY (id_devoir) REFERENCES devoirs(id_devoir) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
 -- 0) Élève : réutiliser si déjà présent, sinon créer
 SELECT id_eleve INTO @eleve_id
 FROM eleves
@@ -127,3 +74,4 @@ WHERE NOT EXISTS (
 );
 
 COMMIT;
+
